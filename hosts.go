4c798b0c214fb1e0eec89a783f9189d43beaa37f
Praveen Ramalingam <pramalingam@anduril.com>
2020-03-29 19:15:42 -0700

    add a searchHost

--- hosts.go
+++ hosts.go
@@ -1,5 +1,10 @@
 package datadog
 
+import (
+	"fmt"
+	"net/url"
+)
+
 type HostActionResp struct {
 	Action   string `json:"action"`
 	Hostname string `json:"hostname"`
@@ -47,4 +52,60 @@ func (client *Client) GetHostTotals() (*HostTotalsResp, error) {
 		return nil, err
 	}
 	return &out, nil
+}
+
+// QueryHosts defines response to GET /v1/hosts/.
+type HostSearchResp struct {
+	TotalReturned *int             `json:"total_returned"`
+	TotalMatching *int             `json:"total_matching"`
+	HostListEntry *[]HostListEntry `json:"host_list"`
+}
+
+type HostListEntry struct {
+	LastReportedTime string             `json:"last_reported_time"`
+	Name             string             `json:"name"`
+	IsMuted          bool               `json:"is_muted"`
+	MuteTimeout      int                `json:"mute_timeout"`
+	Apps             []string           `json:"apps"`
+	TagsBySource     map[string]int     `json:"tags_by_source"`
+	Up               bool               `json:"up"`
+	Metrics          map[string]float32 `json:"metrics"`
+	Source           []string           `json:"sources"`
+	HostName         string             `json:"host_name"`
+	Id               int                `json:"id"`
+	Aliases          []string           `json:"aliases"`
+}
+
+//Search among hosts live within the past 2 hours. Max 100 results at a time.
+func (client *Client) QueryHosts(filter, sort_field, sort_dir string, start, count, from int) (HostSearchResp, error){
+	// Since this is a GET request, we need to build a query string.
+	vals := url.Values{}
+
+	if filter != "" {
+		vals.Add("filter", filter)
+	}
+	if sort_field != "" {
+		vals.Add("priority", sort_field)
+	}
+	if sort_dir != "" {
+		vals.Add("sources", sort_dir)
+	}
+	if start != 0 {
+		vals.Add("start", string(start))
+	}
+	if count != 100 {
+		vals.Add("count", string(count))
+	}
+	if from != 0 {
+		vals.Add("from", string(from))
+	}
+
+	// Now the request and response.
+	var out HostSearchResp
+	if err := client.doJsonRequest("GET",
+		fmt.Sprintf("/v1/hosts?%s", vals.Encode()), nil, &out); err != nil {
+		return HostSearchResp{}, err
+	}
+	return out, nil
+
 }
